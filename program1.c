a8455591cd34ef5dd741107952bfd8075a56704d
jstanisz <joanna.stanisz.12@gmail.com>
2016-04-20 10:23:53 +0200

    Added files via upload

---
+++ program1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+int a=75, b=9;
+float iloraz;
+int ilo;
+ilo=a/b;
+iloraz=75./9;
+printf("Na wykonanie kazdego zadania pozostalo w wersji z liczba calkowita pozostalo %d\n",ilo);
+
+printf("Na wykonanie kazdego zadania pozostalo w wersji z liczba rzeczywista pozostalo %f\n",iloraz);
+}
